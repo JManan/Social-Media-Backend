f3ada8ba9b629eb9dd84fe8de1d670c2fbf6da1e
Manan Jain <97405651+JManan@users.noreply.github.com>
2022-07-25 19:50:45 +0530

    Update settings.py

--- dvm/settings.py
+++ dvm/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['blog-dvm.azurewebsites.net', 'localhost']
 
